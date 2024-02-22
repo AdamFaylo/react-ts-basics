5c513ad6a41fd7f0658e57e5564a4e3d3dcab0fb
ADAM FAYLO <112155899+AdamFaylo@users.noreply.github.com>
2024-02-22 07:19:49 +0200

    buildinng a more dynamic & flexible component

--- react-ts-basics/src/components/CousreGoalList.tsx
+++ react-ts-basics/src/components/CousreGoalList.tsx
@@ -1,14 +1,38 @@
 
 import CourseGoal from "./CourseGoal.tsx";
 import { type CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./InputBox.tsx";
+import { type ReactNode } from "react";
 
 type CourseGoalListProps = {
     goals: CGoal[];
     onDeleteGoal: (id: number) => void;
 };
 
-export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
+export default function CourseGoalList({
+    goals,
+    onDeleteGoal,
+}: CourseGoalListProps) {
+    if (goals.length === 0) {
+        return (
+            <InfoBox
+                mode="hint">
+                You have no course gaols yet. Start adding some!
+            </InfoBox>
+        );
+    }
+
+    let warningBox: ReactNode;
+
+    if (goals.length >=4) {
+        warningBox = <InfoBox mode="warning" severity="">
+            You're collection  a lot of goals. Don't put too  on your plante!
+        </InfoBox>
+    }
+
     return (
+    <>
+        {warningBox}
         <ul>
             {goals.map((goal) => (
                 <li key={goal.id}>
@@ -20,5 +44,7 @@ export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListPr
                 </li>
             ))}
         </ul>
+    </>
+
     );
 }
